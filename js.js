79eb5242ae3945f60155d50d12eaecbba5503803
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2025-04-24 18:19:39 +0700

    Edit 95% complete game logic

--- js.js
+++ js.js
@@ -11,21 +11,21 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-getComputerChoice();
-console.log(computerChoice);
+// getComputerChoice();
+// console.log(computerChoice);
 
 function getHumanChoice() {
   humanChoice = prompt("rock paper scissors?");
   return humanChoice;
 }
 
-getHumanChoice();
+// getHumanChoice();
 // console.log(humanChoice);
 
 // console.log(typeof humanAnswer);
 
-let humanScore = 0;
-let computerScore = 0;
+// let humanScore = 0;
+// let computerScore = 0;
 
 let r = "Rock";
 let p = "Paper";
@@ -39,6 +39,9 @@ let sbp = "Scissors beats Paper!";
 let yd = "You Draw!";
 
 // Start of game JS
+
+/*
+
 
 function playRound(humanChoice, computerChoice) {
   // Function and reassignment of Capitalized and lower case choices.
@@ -75,6 +78,66 @@ function playRound(humanChoice, computerChoice) {
   }
 }
 
-playRound(humanChoice, computerChoice);
 
-console.log(humanScore, computerScore);
+
+*/
+
+// PLAY GAME FUNCTION
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  getComputerChoice();
+  getHumanChoice();
+
+  function playRound(humanChoice, computerChoice) {
+    // Function and reassignment of Capitalized and lower case choices.
+    function capitalized(x) {
+      return x[0].toUpperCase() + x.slice(1).toLowerCase();
+    }
+    humanChoice = capitalized(humanChoice);
+    computerChoice = capitalized(computerChoice);
+
+    console.log(humanChoice, computerChoice);
+
+    //   Game Logic start:
+
+    if (humanChoice === computerChoice) {
+      console.log(yd);
+    } else if (humanChoice == r && computerChoice == s) {
+      console.log(yw + rbs);
+      humanScore++;
+    } else if (humanChoice == r && computerChoice == p) {
+      console.log(yl + pbr);
+      computerScore++;
+    } else if (humanChoice == p && computerChoice == r) {
+      console.log(yw + pbr);
+      humanScore++;
+    } else if (humanChoice == p && computerChoice == s) {
+      console.log(yl + sbp);
+      computerScore++;
+    } else if (humanChoice == s && computerChoice == p) {
+      console.log(yw + sbp);
+      humanScore++;
+    } else if (humanChoice == s && computerChoice == r) {
+      console.log(yl + rbs);
+      computerScore++;
+    }
+  }
+
+  playRound(humanChoice, computerChoice);
+  console.log("Human Score: " + humanScore, "Computer Score: " + computerScore);
+
+  if (humanScore >= 5) {
+    console.log("Game over, human wins!");
+  } else if (computerScore >= 5) {
+    console.log("Game over, Computer wins!");
+  } else {
+    getComputerChoice();
+    getHumanChoice();
+    playRound(humanChoice, computerChoice);
+  }
+}
+
+playGame();
