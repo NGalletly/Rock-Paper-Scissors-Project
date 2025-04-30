6611e01086786cb0559c643e84699e77082a4f17
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2025-04-30 14:25:51 +0700

    Edit completed basic UI

--- js.js
+++ js.js
@@ -2,12 +2,20 @@ console.log("Hello World");
 
 let computerChoice = "aa";
 let humanChoice = "";
+let humanScore = 0;
+let computerScore = 0;
+
+// HTML selectors
+rockButton = document.querySelector(".rockButton");
+paperButton = document.querySelector(".paperButton");
+scissorButton = document.querySelector(".scissorButton");
+scoreBoard = document.querySelector(".scoreBoard");
+scoreDisplay = document.querySelector(".scoreDisplay");
 
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 100);
 
   computerChoice = choice <= 33 ? "Rock" : choice <= 66 ? "Paper" : "Scissors";
-  //   console.log(computerChoice);
   return computerChoice;
 }
 
@@ -27,110 +35,71 @@ let rbs = "Rock beats Scissors!";
 let sbp = "Scissors beats Paper!";
 let yd = "You Draw!";
 
-/*
 // playRound function
 
 function playRound(humanChoice, computerChoice) {
-  // Function and reassignment of Capitalized and lower case choices.
-  function capitalized(x) {
-    return x[0].toUpperCase() + x.slice(1).toLowerCase();
-  }
-  humanChoice = capitalized(humanChoice);
-  computerChoice = capitalized(computerChoice);
-
-  console.log(humanChoice, computerChoice);
-
-  //   Game Logic start:
-
   if (humanChoice === computerChoice) {
     console.log(yd);
+    scoreBoard.textContent = yd + " No score added!";
   } else if (humanChoice == r && computerChoice == s) {
     console.log(yw + rbs);
+    scoreBoard.textContent = yw + rbs;
     humanScore++;
+    scoreDisplay.textContent = `Human score: ${humanScore}, Computer score: ${computerScore}`;
   } else if (humanChoice == r && computerChoice == p) {
     console.log(yl + pbr);
+    scoreBoard.textContent = yl + pbr;
     computerScore++;
+    scoreDisplay.textContent = `Human score: ${humanScore}, Computer score: ${computerScore}`;
   } else if (humanChoice == p && computerChoice == r) {
     console.log(yw + pbr);
+    scoreBoard.textContent = yw + pbr;
     humanScore++;
+    scoreDisplay.textContent = `Human score: ${humanScore}, Computer score: ${computerScore}`;
   } else if (humanChoice == p && computerChoice == s) {
     console.log(yl + sbp);
+    scoreBoard.textContent = yl + sbp;
     computerScore++;
+    scoreDisplay.textContent = `Human score: ${humanScore}, Computer score: ${computerScore}`;
   } else if (humanChoice == s && computerChoice == p) {
     console.log(yw + sbp);
+    scoreBoard.textContent = yw + sbp;
     humanScore++;
+    scoreDisplay.textContent = `Human score: ${humanScore}, Computer score: ${computerScore}`;
   } else if (humanChoice == s && computerChoice == r) {
     console.log(yl + rbs);
+    scoreBoard.textContent = yl + rbs;
     computerScore++;
+    scoreDisplay.textContent = `Human score: ${humanScore}, Computer score: ${computerScore}`;
+  }
+
+  if (humanScore >= 5) {
+    scoreBoard.textContent = "Human wins!";
+    scoreDisplay.textContent = "Game Over";
+  } else if (computerScore >= 5) {
+    scoreBoard.textContent = "Computer wins!";
+    scoreDisplay.textContent = "Game Over";
+  } else {
   }
 }
 
-*/
+rockButton.addEventListener("click", (e) => {
+  humanChoice = r;
+  getComputerChoice();
+  console.log(computerChoice);
+  playRound(humanChoice, computerChoice);
+});
 
-// PLAY GAME FUNCTION
+paperButton.addEventListener("click", (e) => {
+  humanChoice = p;
+  getComputerChoice();
+  console.log(computerChoice);
+  playRound(humanChoice, computerChoice);
+});
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  function playRound(humanChoice, computerChoice) {
-    // Function to make both choices uniform for comparison.
-    function capitalized(x) {
-      return x[0].toUpperCase() + x.slice(1).toLowerCase();
-    }
-    humanChoice = capitalized(humanChoice);
-    computerChoice = capitalized(computerChoice);
-
-    console.log(humanChoice, computerChoice);
-
-    //   Game Logic start: Brute force method
-    // Refer to variables above for outputs. Use template literals next time.
-
-    if (humanChoice === computerChoice) {
-      console.log(yd);
-    } else if (humanChoice == r && computerChoice == s) {
-      console.log(yw + rbs);
-      humanScore++;
-    } else if (humanChoice == r && computerChoice == p) {
-      console.log(yl + pbr);
-      computerScore++;
-    } else if (humanChoice == p && computerChoice == r) {
-      console.log(yw + pbr);
-      humanScore++;
-    } else if (humanChoice == p && computerChoice == s) {
-      console.log(yl + sbp);
-      computerScore++;
-    } else if (humanChoice == s && computerChoice == p) {
-      console.log(yw + sbp);
-      humanScore++;
-    } else if (humanChoice == s && computerChoice == r) {
-      console.log(yl + rbs);
-      computerScore++;
-    }
-    console.log(
-      "Human Score: " + humanScore,
-      "Computer Score: " + computerScore
-    );
-  }
-
-  // Logic for playing the game repeatedly until winner is decided
-
-  let n = 0;
-
-  while (n < Infinity) {
-    if (humanScore >= 5) {
-      console.log("Game over, human wins!");
-      break;
-    } else if (computerScore >= 5) {
-      console.log("Game over, Computer wins!");
-      break;
-    } else {
-      getComputerChoice();
-      getHumanChoice();
-      playRound(humanChoice, computerChoice);
-    }
-    n++;
-  }
-}
-
-playGame();
+scissorButton.addEventListener("click", (e) => {
+  humanChoice = s;
+  getComputerChoice();
+  console.log(computerChoice);
+  playRound(humanChoice, computerChoice);
+});
